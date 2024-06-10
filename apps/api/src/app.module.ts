0ab4db5220987c21a57de6c742063b89981418a4
Natchii59 <caron.nathan@hotmail.com>
2024-06-10 23:07:40 +0200

    feat(api): add graphql module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,44 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+import { ApolloDriver } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 
-import { AppController } from './app.controller'
+import { AppResolver } from './app.resolver'
 import { AppService } from './app.service'
 
+import type { ApolloDriverConfig } from '@nestjs/apollo'
+import type { HttpException } from '@nestjs/common'
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: 'src/graphql/schema.gql',
+      sortSchema: true,
+      introspection: true,
+      formatError: err => {
+        const originalError = err.extensions?.originalError as
+          | HttpException
+          | undefined
+
+        if (originalError) {
+          return {
+            ...originalError,
+            path: err.path
+          }
+        }
+
+        return err
+      }
+    })
+  ],
+  providers: [AppService, AppResolver]
 })
 export class AppModule {}
--- apps/api/src/app.resolver.ts
+++ apps/api/src/app.resolver.ts
@@ -1,13 +1,13 @@
-import { Controller, Get } from '@nestjs/common'
+import { Query, Resolver } from '@nestjs/graphql'
 
 import { AppService } from './app.service'
 
-@Controller()
-export class AppController {
+@Resolver()
+export class AppResolver {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Query(() => String)
+  hello(): string {
     return this.appService.getHello()
   }
 }
