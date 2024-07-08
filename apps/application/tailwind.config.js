039f1032007fe6287e3bb6a412e5c45e7ddfe753
geoffrey <geoffrey02@orange.fr>
2024-07-08 10:50:09 +0200

    Revert "Fix/nativewind (#35)"
    This reverts commit 33e338431936e61dae2b0a3956ab345056a11dc3.

--- apps/application/tailwind.config.js
+++ apps/application/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ["./App.{js,jsx,ts,tsx}", "./</apps/application>/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
