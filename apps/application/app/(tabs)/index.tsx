648366de60e053ec637731d9c06d8644553e301c
Benjamin <benjamin.kliewer@epitech.eu>
2024-07-11 19:54:46 +0200

    fix:reactnative

--- apps/application/app/(tabs)/index.tsx
+++ apps/application/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>Home</Text>
     </SafeAreaView>
+
+
   )
 }
+
--- apps/application/tailwind.config.js
+++ apps/application/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./</apps/application>/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    './**/*.{js,tsx,ts,jsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
